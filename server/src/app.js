cf172a631efd125c4257324b83f65955e4617536
iashutosh01 <kashutosh0099@gmail.com>
2026-07-26 21:58:19 +0530

    deploy fix

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  process.env.CLIENT_ORIGIN,
+  env.CLIENT_ORIGIN,
 ].filter(Boolean);
 
 app.use(
